f2ee9a998f5e9edea7d2e94976d78b714f9190be
MatiRosen <matias.rosenstein00@gmail.com>
2023-05-17 22:52:57 -0300

    a

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,7 @@
 <template>
-    <h1>{{ obtenerSucesionDeFibonacci(10) }}</h1>
+    <h1>Hola!</h1>
 </template>
 
 <script setup>
-const obtenerSucesionDeFibonacci = (n) => {
-    let sucesion = [0, 1];
-    for (let i = 2; i < n; i++) {
-        sucesion[i] = sucesion[i - 2] + sucesion[i - 1];
-    }
-    return sucesion;
-};
+
 </script>
--- src/views/creador/HomeView.vue
+++ src/views/creador/HomeView.vue
@@ -1,3 +1,3 @@
 <template>
-  
+  <h1>Chau!</h1>
 </template>
