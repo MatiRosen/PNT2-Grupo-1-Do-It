b8ecb535d82e31e1047c766c085b4acba053cdfb
Francisco <franciscolagaresgarcia@gmail.com>
2023-06-05 00:45:20 -0300

    chat mejorado

--- front-end/src/views/ChatView.vue
+++ front-end/src/views/ChatView.vue
@@ -1,19 +1,50 @@
 <template>
-    mensajes
-    <div v-for="m in chat.mensajes">
-        {{ m.contenido }}
+<div class="ideas"></div>
+    
+<section id="ideas">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 v-for="p in chat.participantes"><span v-if="p != user.id">Chat con {{ otherUser.nombre }} </span></h2>
+        </div>
+      </div>
+      <div class="col-md-8 ">
+        <div class="row my-1 " v-for="m in chat.mensajes">
+          <div class="card shadow-lg">
+              <div class="card-block position-relative">
+                <div class="row">
+                  <div class="col-md-12 ">
+                      <h2>
+                        <div v-if="m.emisor == user.id" class="subtituloRojo">
+                          {{ m.contenido }}
+                        </div>
+                        <div v-else class="subtitulovioleta">
+                          {{ m.contenido }}
+                        </div>
+                      </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <form @submit.prevent="mandarMensaje(contenido,chat.id)">
+            <input type="text" v-model="contenido">
+            <button type="submit">mandar mensaje</button>
+        </form>
+      </div>
     </div>
-    <form @submit.prevent="mandarMensaje(contenido,chat.id)">
-                      <input type="text" v-model="contenido">
-                      <button type="submit">mandar mensaje</button>
-                  </form>
+  </section>
+
+
+
 </template>
 
 <script setup>
 import { useChatStore } from "../stores/chat";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useUserStore } from "../stores/user";
+import service from "../services/userService";
 
 const storeUser = useUserStore();
 const { user } = storeUser;
@@ -31,4 +62,118 @@ const mandarMensaje = (contenido, chatId) =>{
     chatStore.mandarMensaje(chatId, mensajeMandar)
 }
 
+const otherUser = ref('')
+
+const getOtherUser = async () => {
+    let otherUserId = chat.participantes.find(p => p != user.id)
+    otherUser.value = (await service.obtenerUsuario(otherUserId)).data
+    console.log(otherUser)
+}
+
+onMounted(getOtherUser)
 </script>
+
+<style scoped>
+
+ .ideas{
+  background: url("../assets/fondo-contacto.png");
+  background-position: center;
+  position: absolute;
+  top: 0;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+
+.titulosgrises {
+  font-weight: bold;
+  color: #6a6a6a;
+  font-size: 20px;
+}
+.subtitulovioleta {
+  font-weight: bold;
+  color: #370a6d;
+  font-size: 17px;
+}
+.subtituloRojo {
+  font-weight: bold;
+  color: #e20000;
+  font-size: 17px;
+}
+.descripcion {
+  font-weight: normal;
+  color: #6a6a6a;
+  font-size: 15px;
+}
+.p {
+  font-size: 12px;
+}
+
+
+.card {
+  border-radius: 20px;
+  border-color: white;
+  width: fit-content;
+  border: 0;
+}
+
+.card-body {
+  padding: 0;
+  margin: 0;
+}
+
+.card-text {
+  padding: 10px;
+  margin: 5px 5px;
+}
+
+.card-title {
+  padding: 0;
+  margin: 0;
+}
+
+.color-gradiente {
+  color: white;
+  padding: 15px;
+  background: rgba(107, 35, 35, 1);
+  background: -moz-linear-gradient(
+    left,
+    rgba(107, 35, 35, 1) 0%,
+    rgba(231, 55, 39, 1) 69%,
+    rgba(231, 55, 39, 1) 100%
+  );
+  background: -webkit-gradient(
+    left top,
+    right top,
+    color-stop(0%, rgba(107, 35, 35, 1)),
+    color-stop(69%, rgba(231, 55, 39, 1)),
+    color-stop(100%, rgba(231, 55, 39, 1))
+  );
+  background: -webkit-linear-gradient(
+    left,
+    rgba(107, 35, 35, 1) 0%,
+    rgba(231, 55, 39, 1) 69%,
+    rgba(231, 55, 39, 1) 100%
+  );
+  background: -o-linear-gradient(
+    left,
+    rgba(107, 35, 35, 1) 0%,
+    rgba(231, 55, 39, 1) 69%,
+    rgba(231, 55, 39, 1) 100%
+  );
+  background: -ms-linear-gradient(
+    left,
+    rgba(107, 35, 35, 1) 0%,
+    rgba(231, 55, 39, 1) 69%,
+    rgba(231, 55, 39, 1) 100%
+  );
+  background: linear-gradient(
+    to right,
+    rgba(107, 35, 35, 1) 0%,
+    rgba(231, 55, 39, 1) 69%,
+    rgba(231, 55, 39, 1) 100%
+  );
+  filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#6b2323', endColorstr='#e73727', GradientType=1);
+}
+
+</style>
