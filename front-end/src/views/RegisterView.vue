782aef136b47c6139c332afdc184dcf03ae2fa84
Matias Rosenstein <matias.rosenstein00@gmail.com>
2023-06-11 19:17:54 -0300

    Proyecto terminado :)

--- front-end/src/views/RegisterView.vue
+++ front-end/src/views/RegisterView.vue
@@ -119,7 +119,7 @@
                                                 type="radio"
                                                 name="Tipo"
                                                 id="creador"
-                                                value="creador"
+                                                value="Creador"
                                                 required />
                                             <label
                                                 class="form-check-label"
@@ -134,8 +134,8 @@
                                                 class="form-check-input mt-1"
                                                 type="radio"
                                                 name="Tipo"
-                                                id="inversor"
-                                                value="inversor"
+                                                id="Inversor"
+                                                value="Inversor"
                                                 required />
                                             <label
                                                 class="form-check-label"
